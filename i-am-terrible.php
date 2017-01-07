50beb2c8340521ea8180589b557fa1efa97e9ad7
Nikki Stevens <nikkilstevens@gmail.com>
2017-01-06 16:00:14 -0800

    Create i-am-terrible.php

---
+++ i-am-terrible.php
@@ -0,0 +1,10 @@
+<?php
+
+// I'm writing this directly in the github gui, so it should be full of syntax errors, in addtion to these terrible ones I'm making on purpose      
+
+function IAMAFUNCTION(int $one) {
+$x=array('one'=>0,'one'=>0, 'one'=>0, 'one'=>0, 'one'=>0, 'one'=>0, 'one'=>0, 'one'=>0 );
+
+return $x=1;
+}
+
